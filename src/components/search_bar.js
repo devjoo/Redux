a0f98ba9929785548ad8fbb97cff686147c58608
sona <chookin77@naver.com>
2017-11-18 01:57:04 +0900

    state, setState

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,24 @@
-import React from 'react';  // React 변수 참조하지 않았어도 항상 인클루드한다
+import React, { Component } from 'react';  // React 변수 참조하지 않았어도 항상 인클루드한다
 
-const searchBar = () => {
-    return <input />    // React.createElement
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { term : '' }
+    }
+
+    render(){
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term : event.target.value})} />
+            </div>
+        );
+    }
 }
 
-export default searchBar;   // 에플리케이션의 어떤 곳에서도 이 searchBar를 불러오게 한다
+export default SearchBar;   // 에플리케이션의 어떤 곳에서도 이 searchBar를 불러오게 한다
 
 
 // class 기반 component는 내부적인 정보를 저장하려할때 사용한다
