c2e1fead3eaacd8a2964cf75ec4ff52fbda7123b
sona <chookin77@naver.com>
2017-11-18 23:32:47 +0900

    YTSearch API video landing state vidio

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAO5xJMDyie6lxkGCRHMDcNlhp2-7DW3W8';
 
-YTSearch({key: API_KEY, term: 'surfboard'}, function (data) {
-    console.log(data);
-});
-
 // Create a new component. This component should produce.
 // Some HTML
 // 스테이트 시작할때 클래스 기반 컴포넌트가 필요하여 리펙토링 진행
 class App extends Component {
-    constructor (prop){
-        super(prop);
+    constructor (props){
+        super(props);
 
-        this.state = { vidios: [] }
+        this.state = { vidios: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboard'}, (vidios) => {
+            this.setState({ vidios });
+            // this.setState({ vidios: vidios });
+        });
     }
 
     render(){
